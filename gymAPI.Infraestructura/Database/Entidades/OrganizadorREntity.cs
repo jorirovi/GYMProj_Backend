d5314f6b94e862d090719d3ad327df9efca4c5d2
Jorge ROMERO <jorirovi@gmail.com>
2024-05-20 16:45:12 +0200

    Modificacion de entidad Organizador Rutinas (OrganizaEntity)

--- gymAPI.Infraestructura/Database/Entidades/OrganizadorREntity.cs
+++ gymAPI.Infraestructura/Database/Entidades/OrganizadorREntity.cs
@@ -2,16 +2,14 @@ using MongoDB.Bson.Serialization.Attributes;
 
 namespace gymAPI.Infraestructura.Database.Entidades
 {
-    public class RutinasgymEntity
+    public class OrganizadorREntity
     {
         [BsonId]
         [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
         public string? iD { get; set; }
-        public string? rutina { get; set; }	
-        public string? imagen  { get; set; }
-        public double peso { get; set; }
-        public string? equipo { get; set; }
-        public int? repeticiones { get; set; }
+        public string? idrutina { get; set; }	
         public string? idusuario { get; set; }
+        public string? idtabata { get; set; }
+        public string? idejercicio { get; set; }	
     }
 }
--- gymAPI.Infraestructura/obj/Debug/net8.0/gymAPI.Infraestructura.AssemblyInfo.cs
+++ gymAPI.Infraestructura/obj/Debug/net8.0/gymAPI.Infraestructura.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("gymAPI.Infraestructura")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a3b39c10d7cd8213858d9b78a44cebc4270c5de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b99958187fc833c70df57ba357b8bd398203fb5c")]
 [assembly: System.Reflection.AssemblyProductAttribute("gymAPI.Infraestructura")]
 [assembly: System.Reflection.AssemblyTitleAttribute("gymAPI.Infraestructura")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
