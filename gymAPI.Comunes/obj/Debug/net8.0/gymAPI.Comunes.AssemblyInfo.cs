b99958187fc833c70df57ba357b8bd398203fb5c
Jorge ROMERO <jorirovi@gmail.com>
2024-05-20 13:55:54 +0200

    Cambios de configuracion de archivos

--- gymAPI.Comunes/obj/Debug/net8.0/gymAPI.Comunes.AssemblyInfo.cs
+++ gymAPI.Comunes/obj/Debug/net8.0/gymAPI.Comunes.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("gymAPI.Comunes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6510a16893b9e9843a21767259278abf6d15cf36")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a3b39c10d7cd8213858d9b78a44cebc4270c5de")]
 [assembly: System.Reflection.AssemblyProductAttribute("gymAPI.Comunes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("gymAPI.Comunes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
