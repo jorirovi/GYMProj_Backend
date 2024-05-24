d7fe89142f7c79354d9be99cbeab83af400d3168
Jorge R. Romero <jorirovi@icloud.com>
2024-05-24 06:16:43 +0200

    se crea metodo para el update del campo password

--- gymAPI.Dominio/Service/GYM/Usuarios/IUsuariosService.cs
+++ gymAPI.Dominio/Service/GYM/Usuarios/IUsuariosService.cs
@@ -1,7 +1,9 @@
+using gymAPI.Infraestructura.Repositorios.Usuarios;
+
 namespace gymAPI.Dominio.Service.GYM.Usuarios
 {
     public interface IUsuariosService
     {
-        
+        Task<UsuarioRepository> UpdatePass(string email);
     }
 }
