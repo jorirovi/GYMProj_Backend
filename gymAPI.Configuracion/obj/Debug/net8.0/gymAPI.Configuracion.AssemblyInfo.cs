472cfc66ebdd677913d408898eb4eb0dcaeb958d
Jorge R. Romero <jorirovi@icloud.com>
2024-06-13 06:11:27 +0200

    ajustes de archivos de la aplicacion

--- gymAPI.Configuracion/obj/Debug/net8.0/gymAPI.Configuracion.AssemblyInfo.cs
+++ gymAPI.Configuracion/obj/Debug/net8.0/gymAPI.Configuracion.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("gymAPI.Configuracion")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8df945cdf50d23672bd0643e9cdfd6882c8af9a0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+146005e06a650d47e9a4bef3df4214abf76a6e0a")]
 [assembly: System.Reflection.AssemblyProductAttribute("gymAPI.Configuracion")]
 [assembly: System.Reflection.AssemblyTitleAttribute("gymAPI.Configuracion")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
